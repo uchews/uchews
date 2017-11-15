dfab0bcbeb5a990b1fb540017deac1e54e6686e8
Katelyn Stamas <cakelyn@gmail.com>
2017-11-15 17:31:50 -0500

    heroku

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://uchews:catdog@ds163745.mlab.com:63745/uchews', { mongoUseClient: true});
+mongoose.connect('mongodb://uchews:catdog@ds163745.mlab.com:63745/uchews', { mongoUseClient: true});
 
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('connected to database');
