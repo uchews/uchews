ac1bf5246ec71e92d525278b680f9cf32a91183c
Raymond Huynh <huynh.r.k@gmail.com>
2017-11-15 20:18:50 -0500

    added login page outline

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,7 +1,15 @@
-const Login = () => {
+const Login = ({clickHandle}) => {
   return (
     <div>
       <h1>Login</h1>
+      <h3>username</h3>
+      <input type="text"/>
+      <h3>password</h3>
+      <input type="text"/>
+      <div>
+        <button onClick={() => clickHandle('home')}>login</button>
+        <button onClick={() => clickHandle('signup')}>signup</button>
+      </div>
     </div>
   )
 }
