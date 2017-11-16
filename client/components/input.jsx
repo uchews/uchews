a3e62f234a951197c2945b833cf28df4322e981f
Katelyn Stamas <cakelyn@gmail.com>
2017-11-15 22:49:01 -0500

    input and types scaffolding

--- client/components/input.jsx
+++ client/components/input.jsx
@@ -3,11 +3,16 @@ const Input = (props) => {
   return (
     <div>
       <form>
-        Location<br />
+        <h4>Location</h4><br />
         <input type="text" name="location" /><br />
-        Number of people<br />
+        <h4>Number of people</h4><br />
         <input type="text" name="peopleNum" /><br />
-        Budget<br />
+        <h4>How far are you willing to travel?</h4><br />
+        <input type="radio" name="radius" value="0" />less than 1 mi<br />
+        <input type="radio" name="radius" value="0" />up to 2 mi<br />
+        <input type="radio" name="radius" value="0" />up to 5 mi<br />
+        <input type="radio" name="radius" value="" />10 mi<br />
+        <h4>Budget</h4><br />
         <input type="checkbox" name="budget" value="$" />$
         <input type="checkbox" name="budget" value="$$" />$$
         <input type="checkbox" name="budget" value="$$$" />$$$
