68bd5c8a999fcaef5a0500ae5c601e5ca097f503
Katelyn Stamas <cakelyn@gmail.com>
2017-11-16 17:44:02 -0500

    fix heroku render

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ class Index extends React.Component {
     super(props);
     this.state= {
       appView: 'home'
-    }
+    };
     this.clickHandle = this.clickHandle.bind(this);
   }
 
@@ -27,42 +27,42 @@ class Index extends React.Component {
           <h1>uChews</h1>
           <Login appView={this.state.appView} clickHandle={this.clickHandle}/>
         </div>
-      );
+      )
     } else if (this.state.appView === 'input') {
       return (
         <div>
           <h1>uChews</h1>
           <Input appView={this.state.appView} clickHandle={this.clickHandle}/>
         </div>
-      );
+      )
     } else if (this.state.appView === 'types') {
       return (
         <div>
           <h1>uChews</h1>
           <Types appView={this.state.appView} clickHandle={this.clickHandle}/>
         </div>
-      );
+      )
     } else if (this.state.appView === 'waiting') {
       return (
         <div>
           <h1>uChews</h1>
           <Waiting appView={this.state.appView} clickHandle={this.clickHandle}/>
         </div>
-      );
+      )
     } else if (this.state.appView === 'results') {
       return (
         <div>
           <h1>uChews</h1>
           <Results appView={this.state.appView} clickHandle={this.clickHandle}/>
         </div>
-      );
+      )
     } else if (this.state.appView === 'signup') {
       return (
         <div>
           <h1>uChews</h1>
           <Signup appView={this.state.appView} clickHandle={this.clickHandle}/>
         </div>
-      );
+      )
     }
   }
 }
