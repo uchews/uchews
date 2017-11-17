e248dee9b749ab97dfe01acd45aa50d0a3f19a63
Raymond Huynh <huynh.r.k@gmail.com>
2017-11-17 14:32:15 -0500

    added styling to sign up

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -17,58 +17,64 @@ const style = {
   box: {
     height: '50%',
     width: '50%',
-    margin: 100,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 300,
     textAlign: 'center',
-    display: 'inline-block',
   },
   button: {
     margin: 10,
-  }
+  },
 };
 
 
 const Signup = ({clickHandle}) => {
   return (
-      <form action="/signup" method="post">
-        <Paper style={style.box}>
-        <AppBar
-          title="Sign Up"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          />
-          <div>
-            <Paper style={style.text} zDepth={1}>
-                <div>
-                  <TextField
-                    hintText="Username Field"
-                    floatingLabelText="Username"
-                    underlineShow={false}
-                  />
-                  <Divider />
-                </div>
-                <div>
-                  <TextField
-                    hintText="Password Field"
-                    floatingLabelText="Password"
-                    type="password"
-                    underlineShow={false}
-                  />
-                  <Divider />
-                </div>
-            </Paper>
-                <div>
-                  <RaisedButton
-                    style={style.button}
-                    label="SIGNUP"
-                  />
-                  <RaisedButton
-                    style={style.button}
-                    label="LOGIN"
-                    onClick={() => clickHandle('login')}
-                  />
-                </div>
-          </div>
-        </Paper>
-      </form>
+    <div>
+      <AppBar
+        title="uChews"
+      />
+        <form action="/signup" method="post">
+          <Paper style={style.box}>
+          <AppBar
+            title="Sign Up"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            />
+            <div>
+              <Paper style={style.text} zDepth={1}>
+                  <div>
+                    <TextField
+                      hintText="Username Field"
+                      floatingLabelText="Username"
+                      underlineShow={false}
+                    />
+                    <Divider />
+                  </div>
+                  <div>
+                    <TextField
+                      hintText="Password Field"
+                      floatingLabelText="Password"
+                      type="password"
+                      underlineShow={false}
+                    />
+                    <Divider />
+                  </div>
+              </Paper>
+                  <div>
+                    <RaisedButton
+                      style={style.button}
+                      label="SIGNUP"
+                    />
+                    <RaisedButton
+                      style={style.button}
+                      label="LOGIN"
+                      onClick={() => clickHandle('login')}
+                    />
+                  </div>
+            </div>
+          </Paper>
+        </form>
+    </div>
   )
 }
 
