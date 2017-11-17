c8cfdafad20c752ac97c0588870df07a1e9a687f
Brian <bbinder912@gmail.com>
2017-11-16 19:38:56 -0500

    Add comments to googlePlacesHelpers

--- server/googlePlacesHelpers.js
+++ server/googlePlacesHelpers.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const Nodegeocoder = require('node-geocoder');
 
 
-const requestRestaurants = function(foodType, latitude, longitude, radius) {
-  const searchURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${foodType}+Restaurant&sensor=true&location=${latitude},${longitude}&radius=${radius}&key=${process.env.GOOGLE_API_KEY}`;
+const requestRestaurants = function(cuisine, latitude, longitude, radius) {
+  const searchURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${cuisine}+Restaurant&sensor=true&location=${latitude},${longitude}&radius=${radius}&key=${process.env.GOOGLE_API_KEY}`;
 
   return axios({
     method: 'get',
@@ -15,12 +15,16 @@ const requestRestaurants = function(foodType, latitude, longitude, radius) {
 
 const handleQueries = function(body, cb) {
   //rankedCuisines = handleRestaurants.rankCuisine(body);
+
+  //dummy data
   rankedCuisines = ['Chinese', 'Italian', 'Greek']
   body = {
     location: '2333 Keara Way, Charlotte, NC 28270',
     budget: 2,
     radius: 500,
   }
+
+  //use Google's geocoder API to transform an address into latitude & longitude
   let geocoder = Nodegeocoder({
     provider: 'google',
     apiKey: process.env.GOOGLE_API_KEY
@@ -29,11 +33,13 @@ const handleQueries = function(body, cb) {
     const lat = locationObject[0].latitude;
     const long = locationObject[0].longitude;
 
+    //map the rankedCuisine array into an array of promises for querying Google Places
     axios.all(rankedCuisines.map((cuisine) => {
       return requestRestaurants(cuisine, lat, long, body.radius);
     }))
       .then((responses) => {
         const restaurants = [];
+        //aggregate the resulting restaurants into a restaurant matrix and add a cuisine property to each restaurant
         for (let i = 0; i < responses.length; i++) {
           restaurants.push(responses[i].data.results);
           restaurants[i].map((restaurant) => {
