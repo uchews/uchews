48d40597ea8f88759baa30487093c55501101723
Raymond Huynh <huynh.r.k@gmail.com>
2017-11-19 11:27:16 -0500

    trying grids out on results page

--- client/src/components/results.jsx
+++ client/src/components/results.jsx
@@ -1,4 +1,34 @@
 import React from 'react';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500
+    height: 450,
+    overflowY: 'auto',
+  }
+}
+
+const tilesData = [
+  {
+    img: 'images/grid-list/00-52-29-429_640.jpg',
+    title: 'Breakfast',
+    author: 'jill111',
+  },
+  {
+    img: 'images/grid-list/burger-827309_640.jpg',
+    title: 'Tasty burger',
+    author: 'pashminu',
+  }
+]
 
 const Results = () => {
   return (
