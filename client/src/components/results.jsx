815d22d0fc705a19e2806a0236225e3e43e231b4
Katelyn Stamas <cakelyn@gmail.com>
2017-11-19 17:42:25 -0500

    results page up and running

--- client/src/components/results.jsx
+++ client/src/components/results.jsx
@@ -3,8 +3,9 @@ import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import Paper from 'material-ui/Paper';
 
-const styles = {
+const style = {
   root: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -14,7 +15,15 @@ const styles = {
     width: 500,
     height: 450,
     overflowY: 'auto',
-  }
+  },
+  paper: {
+    display: 'inline-block',
+    height: '50%',
+    margin: '0 auto',
+    padding: 50,
+    textAlign: 'center',
+    width: '50%'
+  },
 }
 
 const tilesData = [
@@ -30,10 +39,31 @@ const tilesData = [
   }
 ]
 
-const Results = () => {
+const Results = ({ results }) => {
   return (
     <div>
-      <h1>Results</h1>
+      <Paper style={style.paper} zDepth={3}>
+        <h1>#1</h1>
+        <img src="/" />
+        <h2>{results[0][0].name}</h2>
+        (HOURS)
+        (ADDRESS)
+        (PHONE #)
+
+        <h1>#2</h1>
+        <img src="/" />
+        <h2>{results[1][0].name}</h2>
+        (HOURS)
+        (ADDRESS)
+        (PHONE #)
+
+        <h1>#3</h1>
+        <img src="/" />
+        <h2>{results[2][0].name}</h2>
+        (HOURS)
+        (ADDRESS)
+        (PHONE #)
+      </Paper>
     </div>
   )
 }
