61a803a1da261d9cef0b0781927393b514697540
Raymond Huynh <huynh.r.k@gmail.com>
2017-11-19 13:50:48 -0500

    Made login stateful

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -4,6 +4,7 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Divider from 'material-ui/Divider';
 import AppBar from 'material-ui/AppBar';
+import axios from 'axios';
 
 const style = {
   text: {
@@ -25,50 +26,107 @@ const style = {
   }
 };
 
-const Login = ({clickHandle}) => {
-  return (
-    <form action="/login" method="post">
-      <Paper style={style.box}>
-      <AppBar
-        title="Log In"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-        <div>
-          <Paper style={style.text} zDepth={1}>
-              <div>
-                <TextField
-                  hintText="Username Field"
-                  floatingLabelText="Username"
-                  underlineShow={false}
-                />
-                <Divider />
-              </div>
-              <div>
-                <TextField
-                  hintText="Password Field"
-                  floatingLabelText="Password"
-                  type="password"
-                  underlineShow={false}
-                />
-                <Divider />
-              </div>
-          </Paper>
-              <div>
-                <RaisedButton
-                  style={style.button}
-                  label="SIGNUP"
-                  onClick={() => clickHandle('login')}
-                />
-                <RaisedButton
-                  style={style.button}
-                  label="LOGIN"
-                  onClick={() => clickHandle('home')}
-                />
-              </div>
-        </div>
-      </Paper>
-    </form>
-  )
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      floatUser: 'username'
+    }
+    this.onUserChange = this.onUserChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  onUserChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmit() {
+    axios.post('/login', {
+      username: this.state.username,
+      password: this.state.password,
+    })
+      .then((response) => {
+        this.setState({
+          username: '',
+          password: ''
+        })
+        console.log(response.data)
+        if (response.data === false) {
+          this.setState({
+            floatUser: 'incorrect username, try again'
+          })
+        } else  {
+          console.log('successful login')
+
+        }
+      })
+      .catch((err) => {
+        console.log('could not reach server')
+        this.setState({
+            floatUser: 'incorrect username, try again'
+        })
+      })
+  }
+
+
+  render() {
+    return (
+      <form action="/login" method="post">
+        <Paper style={style.box}>
+        <AppBar
+          title="Log In"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          <div>
+            <Paper style={style.text} zDepth={1}>
+                <div>
+                  <TextField
+                    hintText="Username Field"
+                    floatingLabelText={this.state.floatUser}
+                    underlineShow={false}
+                    onChange={this.onUserChange}
+                    value={this.state.username}
+                    name="username"
+                  />
+                  <Divider />
+                </div>
+                <div>
+                  <TextField
+                    hintText="Password Field"
+                    floatingLabelText="Password"
+                    type="password"
+                    underlineShow={false}
+                    onChange={this.onUserChange}
+                    name="password"
+                    value={this.state.password}
+                  />
+                  <Divider />
+                </div>
+            </Paper>
+                <div>
+                  <RaisedButton
+                    style={style.button}
+                    label="SIGNUP"
+                    onClick={() => this.props.clickHandle('signup')}
+                  />
+                  <RaisedButton
+                    style={style.button}
+                    label="LOGIN"
+                    onClick={this.handleSubmit}
+                  />
+                </div>
+          </div>
+        </Paper>
+      </form>
+
+
+    )
+  }
 }
+
 
 export default Login;
