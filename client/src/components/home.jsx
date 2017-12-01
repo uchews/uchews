8a1e19df164aeb45c34d234befb447ec4e56b429
Daniel <dkwonism@gmail.com>
2017-12-01 17:43:36 -0500

    Add Avatar to show in Home component

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -7,6 +7,8 @@ import MenuItem from 'material-ui/MenuItem';
 import axios from 'axios';
 import GroupList from './grouplist.jsx';
 import NewGroup from './newGroup.jsx';
+import FlatButton from 'material-ui/FlatButton';
+import Avatar from 'material-ui/Avatar';
 
 
 // sets styles for material ui components
@@ -28,7 +30,8 @@ const style = {
 
 class Home extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.onFileLoad = this.onFileLoad.bind(this);
   }
 
   componentDidMount() {
@@ -49,15 +52,24 @@ class Home extends React.Component {
     this.setState({open: false});
   }
 
+  onFileLoad(e, file) {
+    console.log(e.target.result, file.name);
+  }
+
 
   render() {
     return (
       <div>
         <Paper style={style.paper} zDepth={3}>
+          <Avatar id="avatar" src="https://avatars1.githubusercontent.com/u/29010046?s=460&v=4"/>
           <h1 style={style.hungry}>Hello {this.props.currentUser}!</h1>
           <h2 style={style.hungry}>Hungry?</h2>
           <RaisedButton style={style.button} primary={true} onClick={ () => this.props.clickHandle('input')} label="Get Started!" />
           <NewGroup />
+
+          <FlatButton label="Choose file" labelPosition="before">
+            <input type="file" />
+          </FlatButton>
         </Paper>
       </div>
     )
