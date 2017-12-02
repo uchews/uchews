5c239132955a8926445e9a39e99e8c58c94210b1
Yazhi Karman <dyazhi@gmail.com>
2017-12-02 11:53:08 -0800

    Switch grouplist to stateless component

--- client/src/components/groupList.jsx
+++ client/src/components/groupList.jsx
@@ -1,36 +1,14 @@
-import React from 'react';
-import axios from 'axios';
-
-class GroupList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      grouplist: []
-    }
-    this.getList = this.getList.bind(this);
-  }
-
-  //send get request
-  /*** need to filter out groups involve current user either in frontend or backend***/
-  getList() {
-    var scope = this;
-    axios.get('/group')
-    .then((res) => {
-      console.log('group list get request succeed');
-      scope.setState({grouplist: res})})
-    .catch((err) => console.log('group list get request error', err))
-  }
-
-  render() {
-    return (<div>
-      {this.state.grouplist.map((group) => {
-        <li>
-          Group: {group.title} Location: {group.location}<br/>
-          Members: {group.members}
-        </li>
-      })}
-    </div>)
-  }
+const GroupList =(props) => {
+  return (<div>
+    <h2>Your Groups:</h2>
+    {props.grouplist.map((group) => (
+      <li>
+        Group: {group.title}
+        Location: {group.location}
+        Members: {group.members}
+      </li>)
+    )}
+  </div>)
 }
 
 export default GroupList;
