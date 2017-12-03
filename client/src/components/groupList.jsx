a04f25d918aa01b7a4e95512170d23789849bf11
Yazhi Karman <dyazhi@gmail.com>
2017-12-03 12:23:56 -0800

    toggle group details

--- client/src/components/groupList.jsx
+++ client/src/components/groupList.jsx
@@ -1,19 +1,49 @@
-import {List, ListItem, ListItemText} from 'material-ui/List';
+import React from 'react';
+import {List, ListItem} from 'material-ui/List';
+import Toggle from 'material-ui/Toggle';
 
-const GroupList =(props) => {
-  return (<div>
-    <h2>Your Groups:</h2>
-    <List>
-      {props.grouplist.map((group) => {
-        let locandmem = `location: ${group.location}
-                         membrs: ${group.member}`
-        return (
-          <ListItem primaryText={group.title}
-          secondaryText={locandmem} />
+class GroupList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      open: false
+    };
+  }
+
+  // state = {
+  //   open: false
+  // };
+
+  // handleToggle() => {
+  //   this.setState({open: !this.state.open,});
+  // }
+
+
+  handleToggle () {
+    this.setState({open: !this.state.open});
+  }
+
+  render() {
+    return (<div>
+      <Toggle
+      type="checkbox" value="off"
+      toggled={this.state.open}
+      onToggle={this.handleToggle} />
+      <h2>Your Groups:</h2>
+      <List>
+        {this.props.grouplist.map((group) => {
+          let locandmem = `location: ${group.location}
+                           members: ${group.member}`
+          return (<ListItem
+            primaryText={group.title} primaryTogglesNestedList={true}
+            nestedItems={[
+              <ListItem secondaryText={locandmem} />
+            ]}/>
           )
-        })}
-    </List>
-  </div>)
+          })}
+      </List>
+    </div>)
+  }
 }
 
 export default GroupList;
