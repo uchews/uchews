217445a1b8f8c764cebbbc86efc0226965ef8a11
Yazhi Karman <dyazhi@gmail.com>
2017-12-03 13:23:19 -0800

    clean up grouplist

--- client/src/components/groupList.jsx
+++ client/src/components/groupList.jsx
@@ -10,30 +10,13 @@ class GroupList extends React.Component {
     };
   }
 
-  // state = {
-  //   open: false
-  // };
-
-  // handleToggle() => {
-  //   this.setState({open: !this.state.open,});
-  // }
-
-
-  handleToggle () {
-    this.setState({open: !this.state.open});
-  }
-
   render() {
     return (<div>
-      <Toggle
-      type="checkbox" value="off"
-      toggled={this.state.open}
-      onToggle={this.handleToggle} />
       <h2>Your Groups:</h2>
       <List>
         {this.props.grouplist.map((group) => {
           let locandmem = `location: ${group.location}
-                           members: ${group.member}`
+                           members: ${group.member}`;
           return (<ListItem
             primaryText={group.title} primaryTogglesNestedList={true}
             nestedItems={[
