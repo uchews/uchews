184479cbaeffb6e45493cc9910d1bc769a5d94f8
Daniel <dkwonism@gmail.com>
2017-12-04 03:20:54 -0500

    Change group listing format and add button for result

--- client/src/components/groupList.jsx
+++ client/src/components/groupList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
 import Toggle from 'material-ui/Toggle';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 class GroupList extends React.Component {
   constructor(props) {
@@ -8,11 +10,29 @@ class GroupList extends React.Component {
     this.state= {
       open: false
     };
+    this.groupList = this.groupList.bind(this);
+  }
+
+  groupList() {
+    return (
+      <div>
+      {this.props.grouplist.map((group) => {
+        return (
+          <div id="group">
+            <h4>group name: {group.title}</h4>
+            <h5>members: {group.members}</h5>
+            <RaisedButton label="Result" default={true} onClick={() => this.props.clickHandle("waiting")} />
+          </div>
+        )
+      })}
+      </div>
+    )
   }
 
   render() {
     return (<div>
-      <h2>These are your CHEWY groups</h2>
+      {this.groupList()}
+      {/*<h2>These are your CHEWY groups</h2>
       <List>
         {this.props.grouplist.map((group) => {
           let locandmem = `location: ${group.location}
@@ -24,7 +44,7 @@ class GroupList extends React.Component {
             ]}/>
           )
           })}
-      </List>
+      </List>*/}
     </div>)
   }
 }
